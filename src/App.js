e111f7051a651ad7caf1dec63bf7673efd2b78cb
UsamabSadiq <usamabsadiq@gmail.com>
2023-08-26 03:05:56 +0500

    header completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div>
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { TbSearch } from 'react-icons/tb';
+import { CgShoppingCart } from 'react-icons/cg';
+import { AiOutlineHeart } from 'react-icons/ai';
+
+import Search from './Search/Search';
+import Cart from '../Cart/Cart'
+import { Context } from '../../utils/context'
+
+const Header = () => {
+    const [scroll, setScroll] = useState(false)
+
+    const handleScroll = () => {
+        const scrollValue = window.scrollY
+        if (scrollValue > 200) {
+            setScroll(true)
+        } else {
+            setScroll(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+    }, [])
+
+    return (
+        <>
+            <div className={`main bg-black text-white w-full h-14 z-50 ${scroll ? ' sticky top-0' : ""}`} >
+                <div className="nav-content max-w-[1200px] mx-auto grid grid-cols-2 md:grid-cols-3 md:px-6 pt-3 place-items-center">
+                    <ul className="left hidden md:flex gap-6 items-center ">
+                        <li className="text-sm font-semibold uppercase cursor-pointer">Home</li>
+                        <li className="text-sm font-semibold uppercase cursor-pointer">About</li>
+                        <li className="text-sm font-semibold uppercase cursor-pointer">Category</li>
+                    </ul>
+                    <div className="center text-2xl font-bold cursor-pointer uppercase">jsDevStore</div>
+                    <div className="right flex gap-3 items-center">
+                        <TbSearch className="text-xl cursor-pointer" />
+                        <AiOutlineHeart className="text-xl cursor-pointer" />
+                        <span className="relative">
+                            <CgShoppingCart className="text-xl cursor-pointer" />
+                            <span className="absolute top-[-5px] right-[-12px] min-w-[20px] text-center bg-[#8e2de2] text-xs rounded-lg">5</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
+
+
+
+
